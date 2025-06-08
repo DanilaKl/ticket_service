f63c65b76559c918da6c6d88ff92aa132a03c1c5
d.klimkin <d.klimkin@edu.centraluniversity.ru>
2025-06-08 23:26:28 +0300

    remove config annotations

--- week_12/hometask/ticket_service/src/main/java/hometask/ticket_service/configuration/AppConfiguration.java
+++ week_12/hometask/ticket_service/src/main/java/hometask/ticket_service/configuration/AppConfiguration.java
@@ -1,11 +1,8 @@
 package hometask.ticket_service.configuration;
 
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 @ConfigurationProperties(prefix = "app")
 public class AppConfiguration {
     private int confirmationTimeoutMinutes;
